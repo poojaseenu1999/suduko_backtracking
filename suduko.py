0affc51f6b153d789e31a1e9a99fbc145a92bdaa
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-30 00:59:41 +0530

    function for valid

--- suduko.py
+++ suduko.py
@@ -10,22 +10,48 @@ board = [
       [0,4,9,2,0,6,0,0,7]
 ]
 
+def valid(bo,num,pos):
+    #check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == 0 and pos[1] != i:
+            return False
+
+    #check col
+    for j in range(len(bo)):
+        if bo[i][pos[1]] == 0 and pos[0] != i:
+            return False
+
+    #check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3 , box_x*3):
+        for j in range(box_x*3 , box_y*3):
+            if bo[i][j] == num and (i,j) == pos:
+                return False
+return True
+
+
+
+
+
+
 def print_borad(bo):
    for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
           print("- - - -  - - - - - - - - - - -   ")
-      
+
         for j in range(len(bo)):
            if j % 3 ==0 and j != 0:
                 print("|" , end =" ")
-         
+
            if j == 8:
               print(bo[i][j])
-            
+
            else:
               print(bo[i][j] , " " , end = "")
-              
-              
+
+
 print_borad(board)
 
 def find_empty(bo):
@@ -33,4 +59,3 @@ def find_empty(bo):
         for j in range(len(bo)):
              if  bo[i][j] == 0:
                  return (i,j) #row,col
-           
